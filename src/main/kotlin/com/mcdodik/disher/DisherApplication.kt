2a14c93ecab03a98a50181fd09aac29121da4690
Алексей Каспшицкий <a.kaspshitskii@bft.ru>
2025-09-10 13:47:02 +0500

    init

---
+++ src/main/kotlin/com/mcdodik/disher/DisherApplication.kt
@@ -0,0 +1,11 @@
+package com.mcdodik.disher
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DisherApplication
+
+fun main(args: Array<String>) {
+    runApplication<DisherApplication>(*args)
+}
---
+++ src/test/kotlin/com/mcdodik/disher/DisherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mcdodik.disher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DisherApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
